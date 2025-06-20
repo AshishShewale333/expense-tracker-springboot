cc36eb5c3a9edbeb3bd5a504a29618676ad325ad
ashish <ashishshewale361998@gmail.com>
2025-06-20 11:03:35 +0530

    adding pagination

--- src/main/java/com/expensetrackerapi/controller/ExpenseController.java
+++ src/main/java/com/expensetrackerapi/controller/ExpenseController.java
@@ -3,6 +3,8 @@ package com.expensetrackerapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,8 +26,8 @@ public class ExpenseController {
 	ExpenseService expenseService;
 	
 	   @GetMapping("/expenses")
-	    public ResponseEntity<List<Expense>> getExpenses() {
-	        List<Expense> expenses = expenseService.getAllExpenses();
+	    public ResponseEntity<List<Expense>> getExpenses(Pageable page) {
+	        List<Expense> expenses = expenseService.getAllExpenses(page).toList();
 	        return ResponseEntity.ok(expenses);
 	    }
 
